4d8a60312465ba92085475ffce1e9794a43d1a69
George B <george@wizardofweb.com>
2022-01-06 09:28:55 +0800

    Add: Room Selector

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import DailyInfoCard from "../components/DailyInfoCard";
 import Header from "../components/Header";
 import style from "../style/home.module.css";
 import RoomInfoCard from "../components/RoomInfoCard";
 import { AiOutlinePlus } from "react-icons/ai";
 import { BsGrid3X3Gap } from "react-icons/bs";
+import data from "../data/roomCard.json";
+import BottomSheet from "../components/BottomSheet";
 
 export default function Home() {
+  const [itemsVisible, setItemsVisible] = useState(true);
+  const [sheetVisible, setSheetVisible] = useState(false);
+  const [sheetCreateRoom, setSheetCreateRoom] = useState(false);
+  const [loaderVisibility, setLoaderVisibility] = useState(false);
+  const [cardId, setcardId] = useState(1);
   return (
     <>
       <Header />
@@ -15,7 +22,7 @@ export default function Home() {
         <RoomInfoCard />
       </div>
       <div className={style.action_btn}>
-        <button>
+        <button onClick={() => setSheetVisible(true)}>
           <AiOutlinePlus className="mr-2" />
           Start a room
         </button>
@@ -23,6 +30,20 @@ export default function Home() {
           <BsGrid3X3Gap />
         </button>
       </div>
+      <BottomSheet
+        sheetTitle="start room"
+        setSheetVisible={item => setSheetVisible(item)}
+        sheetVisible={sheetVisible}
+        cardDetail={data.find(item => item.id == cardId)}
+        setItemsVisible={item => setItemsVisible(item)}
+        setSheetCreateRoom={item => {
+          setLoaderVisibility(true);
+          setTimeout(() => {
+            setSheetCreateRoom(item);
+            setLoaderVisibility(false);
+          }, 1000);
+        }}
+      />
     </>
   );
 }
