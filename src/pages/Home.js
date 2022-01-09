34c7c06783e38544729c2f4576c0537c26248b75
George B <george@wizardofweb.com>
2022-01-09 14:18:51 +0800

    Added: Loader

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,6 +16,24 @@ export default function Home() {
   const [cardId, setcardId] = useState(1);
   return (
     <>
+      {loaderVisibility ? (
+        <div
+          style={{
+            position: "fixed",
+            top: "0",
+            right: "0",
+            bottom: "0",
+            left: "0",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <img src="/images/loader.gif" alt="" />
+        </div>
+      ) : (
+        ""
+      )}
       <Header />
       <div className={style.home_container}>
         <DailyInfoCard />
